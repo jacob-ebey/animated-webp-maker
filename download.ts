6451e3d2ca9fae425725d2e120f416c6ba114722
Jacob Ebey <jacob.ebey@live.com>
2025-02-27 19:28:33 -0800

    move files

--- download.ts
+++ download.ts
@@ -1,6 +1,6 @@
 import * as fs from "node:fs";
 
-fs.mkdirSync("images", { recursive: true });
+fs.mkdirSync("public/images", { recursive: true });
 
 const imgUrl = (step: string) =>
   `https://cdn.shopify.com/s/files/1/0655/4127/5819/files/rotating_sweatshirt_${step}.webp?v=1740686102?width=1600&height=900&crop=center`;
@@ -15,7 +15,7 @@ for (let i = 1; i <= 60; i++) {
   }
 
   const buffer = await response.arrayBuffer();
-  fs.writeFileSync(`./images/${step}.webp`, Buffer.from(buffer));
+  fs.writeFileSync(`./public/images/${step}.webp`, Buffer.from(buffer));
   process.stdout.write(".");
 }
 console.log(" Download complete");
